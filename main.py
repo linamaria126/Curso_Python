d71ecaefd88376b8a32425e85e3cca2dfe8f6efc
Lina María María <linamaria126@gmail.com>
2021-01-28 20:15:11 +0000

    Ejercicios Python

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-#print("Hola mundo")
+# Ejercicio 1.
+#print("Digita una frase")
+#x = input ()
+#print("Digitaste:  " + x)
 
-print("Hola mundo")
+
+# Ejercicio 2. 
+#print("Digita tu nombre")
+#nombre = input()
+#print("Hola " + nombre + "!")
+
+
+# Ejercicio 3. 
+#print("Digita un primer número")
+#n1 = int(input())
+#print("Digita un segúndo número")
+#n2 = int(input())
+#n3 = n1 + n2
+#print("La suma de los dos números es: " + str(n3))
+
+# Ejercicio 4.
+#print("Digita tu año de nacimiento")
+#año_nacimiento = int(input())
+#año_actual = 2021
+#edad = año_actual - año_nacimiento
+#print("Tienes " + str(edad) + " años")
+
+# Ejercicio 5
+print("Ingrese la base del rectángulo")
+base = int(input())
+print("Ingrese la altura del rectángulo")
+altura = int(input())
+area = base * altura
+print ("El área del triángulo es", area)
